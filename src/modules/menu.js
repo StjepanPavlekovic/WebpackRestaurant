39360edfef301116e26d6fd48210bc17ed3395cb
Stjepan Pavlekovic <spavlekovic@zsis.hr>
2024-01-08 15:28:17 +0100

    Publish prepare

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,37 +1,37 @@
 function loadMenu(displayElement) {
   const menuItems = [
     {
-      imgSrc: "../src/public/burger.jpg",
+      imgSrc: "./src/public/burger.jpg",
       title: "AI-catching burger",
       description:
         "This burger is like no other - literally. Soft buns, juicy meat, melted cheese... What more can you want?",
     },
     {
-      imgSrc: "../src/public/fish.jpg",
+      imgSrc: "./src/public/fish.jpg",
       title: "Oven-baked sAImon",
       description:
         "Juicy, tender, delicious... Done just right, coupled with finest herbs and dressings.",
     },
     {
-      imgSrc: "../src/public/pasta.jpg",
+      imgSrc: "./src/public/pasta.jpg",
       title: "Pasta AI-dente",
       description:
         "Soft on the outside with a bite in the center. This pasta cooked to perfection will get you as close as it gets to the authentic experience of AItaly.",
     },
     {
-      imgSrc: "../src/public/pizza.jpg",
+      imgSrc: "./src/public/pizza.jpg",
       title: "Mama mAIa!",
       description:
         "Beautiful crust, fresh san marzano tomatoes, mozzarella and fresh basil. Nothing more to say.",
     },
     {
-      imgSrc: "../src/public/steak.jpg",
+      imgSrc: "./src/public/steak.jpg",
       title: "RibAI steak",
       description:
         "This prime cut of meat will leave you wanting more. Done to perfection, served with a beautiful mix of greens. Goes well with our red wines.",
     },
     {
-      imgSrc: "../src/public/tiramisu.jpg",
+      imgSrc: "./src/public/tiramisu.jpg",
       title: "Neuramisu",
       description:
         "Pride and joy of our dessert menu. Bitter-sweet combo from heaven, right here in our cooler - ready to be served.",
